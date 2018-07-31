dd55b9caa47e911d5f15bbb280eecc4d04101d01
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-31 12:02:18 +0200

    exo4p7 php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <!--Création d'un formulaire simple avec post en méthode-->
+<form class="formDeQualité" action="user.php" method="post">
+  <label for="lastName">Votre nom: </label><input type="text" name="lastName" placeholder="nom" />
+  <label for="firstName">Votre prénom: </label><input type="text" name="firstName" placeholder="prénom" />
+  <input type="submit" name="submit" />
+</form>
+  </body>
+</html>
---
+++ user.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 4</title>
+  </head>
+  <body>
+    <p>
+<?php
+  echo $_POST['lastName'] . ' ' . $_POST['firstName'];
+?>
+    </p>
+  </body>
+</html>

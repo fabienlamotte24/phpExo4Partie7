b8aae2d68c23ff39029b2ae9806534578d0d9d8b
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-21 14:28:14 +0200

    Correction avec la classe

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
   </head>
   <body>
     <!--Création d'un formulaire simple avec post en méthode-->
-<form class="formDeQualité" action="user.php" method="post">
-  <label for="lastName">Votre nom: </label><input type="text" name="lastName" placeholder="nom" />
-  <label for="firstName">Votre prénom: </label><input type="text" name="firstName" placeholder="prénom" />
+<form action="user.php" method="POST">
+  <label for="lastName">Votre nom: </label> <input type="text" name="lastName" placeholder="nom" />
+  <label for="firstName">Votre prénom: </label> <input type="text" name="firstName" placeholder="prénom" />
   <input type="submit" name="submit" />
 </form>
   </body>
--- user.php
+++ user.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice 4</title>
-  </head>
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice 4</title>
+</head>
   <body>
     <p>
-<?php
-  echo $_POST['lastName'] . ' ' . $_POST['firstName'];
-?>
+      <!--Création d'une condition pour vérifier l'existence des données enregistréelse
+      Puis affichage de celle-ci avec echo dans une phrase-->
+      <?php
+      if(!empty($_POST['lastName']) && !empty($_POST['firstName'])){
+        echo 'Je m\'appelle ' . $_POST['firstName'] . ' ' . $_POST['lastName'];
+      }
+      ?>
     </p>
   </body>
 </html>
